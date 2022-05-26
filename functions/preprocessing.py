6f74be285a5c4742c6a994ad49a9cdb923c8c1d8
Teodora Srećković <teodorasrec@gmail.com>
2022-05-26 11:49:30 +0200

    Update preprocessing.py

--- functions/preprocessing.py
+++ functions/preprocessing.py
@@ -2,7 +2,8 @@
 
 def preprocessing(data):
     data = data/4095
-    range_dopler_arr = processing.processing_rangeDopplerData(data[:, 0, :, :])
+    range_dopler_arr = np.abs(processing.processing_rangeDopplerData(data[:, 0, :, :]))
     for i in range(1, 3):
-        range_dopler_arr += processing.processing_rangeDopplerData(data[:, i, :, :])
+        range_dopler_arr += np.abs(processing.processing_rangeDopplerData(data[:, i, :, :]))
+    
     return range_dopler_arr
